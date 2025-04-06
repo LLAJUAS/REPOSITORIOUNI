415218a6d360a21ea727a3daf39e9f28800c9253
Paye7w7 <145301425+Paye7w7@users.noreply.github.com>
2025-04-06 16:16:45 -0400

    actualizadp conexion.php

--- REPOSITORIO/conexion.php
+++ REPOSITORIO/conexion.php
@@ -1,24 +1,19 @@
 <?php
-$conexion = conexionPHP();
-
-if ($conexion) {
-    echo "Conexión exitosa a la base de datos.";
-} else {
-    echo "Error en la conexión.";
-}
-
-function conexionPHP(){
-    $server = "localhost";
-    $user = "root";
-    $pass = "62397902";
-    $db = "unifranz_db";
-
-    $conectar = mysqli_connect($server, $user, $pass, $db);
-
-    if (!$conectar) {
-        die("Error en la conexión: " . mysqli_connect_error());
+function obtenerConexion() {
+    static $conexion = null; 
+    if ($conexion === null) {
+        $host = '127.0.0.1'; 
+        $port = 3306;
+        $database = 'unifranz_db';
+        $username = 'root';
+        $password = ''; /*(ponen su conexión en el caso que tengan)
+*/
+        $conexion = new mysqli($host, $username, $password, $database, $port);
+        if ($conexion->connect_error) {
+            die("Error de conexión: " . $conexion->connect_error);
+        }
+        $conexion->set_charset("utf8"); 
     }
-
-    return $conectar;
+    return $conexion;
 }
 ?>
